82537c9d028574d6148a005a55a0288252f5f267
Allan M Alegretti <allanalegretti@gmail.com>
2022-05-28 17:07:53 -0300

    Organizando arquivos e avisos de erro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import axios from 'axios';
 import * as S from './styled';
 // import { useHistory } from 'react-router-dom';
@@ -8,9 +8,11 @@ function App(props) {
     // const history = useHistory();
     let navigate = useNavigate();
     const [ usuario, setUsuario ] = useState('');
+    const [ erro, setErro ] = useState(false);
 
     function handlePesquisa() {
-        axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+        axios.get(`https://api.github.com/users/${usuario}/repos`)
+        .then(response => {
             const repositories = response.data;
             const respositoriesName = [];
             // console.log(response.data);
@@ -21,15 +23,22 @@ function App(props) {
             // console.log(respositoriesName);
             localStorage.setItem('repositoriesName', JSON.stringify(respositoriesName));
             // history.push('/repositories');
+            setErro(false);
             navigate('/repositories');
+        })
+        .catch(erro => {
+            setErro(true);
         });
     }
 
     return (
-        <S.Container>
-            <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
-            <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-        </S.Container>
+        <S.HomeContainer>
+            <S.Content>
+                <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+                <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+            </S.Content>
+                {erro ? <S.ErrorMsg>Ocorreu um erro, tente novamente.</S.ErrorMsg> : '' }
+        </S.HomeContainer>
     )
 }
 
